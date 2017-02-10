a03b704ba85ace64f8f5a8b5f0f6c1001b801d1d
jb <jenniferbeem@gmail.com>
2017-02-09 20:53:23 -0800

    add user input for new contact

--- app/app.php
+++ app/app.php
@@ -4,15 +4,28 @@
     require_once __DIR__."/../src/address.php";
 
     session_start();
+
     if (empty($_SESSION['address_list'])) {
         $_SESSION['address_list'] = array();
     }
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/../view'
+    ));
 
     $app["debug"] = true;
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('root.html.twig');
+    });
 
+    $app->post("/add", function() use ($app) {
+        $name_input = $_POST['name'];
+        $phone_input = $_POST['phone'];
+        $address_input = $_POST['amount'];
+        
+    });
 
 
 
