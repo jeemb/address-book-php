2af6d9a7acbba01aa1592e3749a2283ac1addd93
jb <jenniferbeem@gmail.com>
2017-02-09 20:23:29 -0800

    add Contact object and get/set functions

--- src/address.php
+++ src/address.php
@@ -5,7 +5,27 @@
         private $phone;
         private $address;
 
-        function __construct()
+        function __construct($name, $phone, $address)
+        {
+            $this->name = $name;
+            $this->phone = $phone;
+            $this->address = $address;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+
+        function getAddress()
+        {
+            return $this->Address;
+        }
 
 
     }
